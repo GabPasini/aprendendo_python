117b76e3e98120f49a9c42438ce30b87916bb18d
GabPasini <jeleia64@gmail.com>
2025-08-20 12:59:12 -0300

    utilização de variáveis e comando de exibição

---
+++ um_nome_aleatorio.py
@@ -0,0 +1,23 @@
+# variaveis ai do meu persoangem ficticio vulgo minha esquizofrenia
+
+nome = "jeleia"
+idade = 5364
+altura = 1.59
+situacaoTrabalho = True
+
+# to aprendendo print, olha :D
+
+print('oi world!')
+print('exemplo de soma:', 3+5)
+print('exemplo de subtração{}'.format (4-2))
+print('exemplo de multiplicação:', 3*5)
+print('exemplo de divisão', 8/2)
+
+ # mais usados
+
+print('exemplo de soma:', 3+5)
+print('exemplo de multiplicação{}'.format (4*2))
+print(f'exemplo de divisão {8/2}' )
+
+print(f'Seu nome eh {nome}, vc tem {idade} anos, por incrivel que pareca, voce tem {altura},e sua situação de trabalho eh {situacaoTrabalho}')
+      
